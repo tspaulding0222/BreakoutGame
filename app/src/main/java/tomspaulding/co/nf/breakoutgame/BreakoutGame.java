b06f9f289e997394857a5cbbbf5d36489d7692f3
Tom Spaulding <tom.spaulding@realart.com>
2015-08-28 15:47:06 -0400

    initial commit

---
+++ app/src/main/java/tomspaulding/co/nf/breakoutgame/BreakoutGame.java
@@ -0,0 +1,14 @@
+package tomspaulding.co.nf.breakoutgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BreakoutGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breakout_game);
+    }
+
+}
